9f5e1004b275d406b669345667554076a0736cff
Jie Li <jie.li@aecom.com>
2025-07-09 09:13:41 -0400

    updated icons

--- src/components/leaflet-map/leaflet-map-container.tsx
+++ src/components/leaflet-map/leaflet-map-container.tsx
@@ -3,16 +3,17 @@ import { LeafletMap } from "./leaflet-map";
 import { useMemo, useState } from "react";
 import { CENTER_LAT, CENTER_LNG, MAX_BOUNDS } from "../../consts/map";
 import { FEET_IN_MILE } from "../../consts/factors";
+import { Icon } from '@mui/material';
 
 export function LeafletMapContainer() {
   const [distanceFt, setDistanceFt] = useState<number | null>(null);
 
   const displayDistanceString = useMemo(() => {
-    if (distanceFt === null) return null;
+    if (distanceFt === null) return "- ft";
     if (distanceFt <= FEET_IN_MILE) {
       return `${Math.round(distanceFt).toLocaleString()} ft`;
     }
-    return `${(distanceFt / FEET_IN_MILE).toLocaleString()} miles`;
+    return `${(Math.round(distanceFt * 100 / FEET_IN_MILE) / 100).toLocaleString()} miles`;
   }, [distanceFt]);
 
   return (
@@ -28,12 +29,12 @@ export function LeafletMapContainer() {
       >
         <LeafletMap setDistanceFt={setDistanceFt} />
       </MapContainer>
-      <div className="absolute opacity-85 rounded-lg p-2 left-[12px] top-[12px] bg-white z-400 flex flex-col gap-2 items-end max-w-[14.5rem]">
-        <div className="text-xs flex items-center gap-1">
-          <span className="material-symbols-outlined">left_click</span>How far
-          am I from the temporary drawdown shoreline? Click on map.
+      <div className="absolute opacity-85 rounded-lg py-2 px-3 left-[12px] top-[12px] bg-white z-400 flex gap-2 items-center justify-between">
+        <div className="text-xs flex items-center gap-2 w-[14rem]">
+          <Icon className="text-[1.2rem]">ads_click</Icon>
+          How far am I from the temporary drawdown shoreline? Click on map.
         </div>
-        {displayDistanceString !== null && <div className="font-semibold text-xl">{displayDistanceString.toLocaleString()}</div>}
+        {displayDistanceString !== null && <div className="font-semibold text-xl flex justify-end w-[5rem]">{displayDistanceString.toLocaleString()}</div>}
       </div>
     </section>
   );
